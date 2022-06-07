77a06171b84a614f2fb6e79025d95f8103b8a3f5
Edsey omenda <omendajames0317@gmail.com>
2022-06-07 18:56:44 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 15.5;
+const num2 = 4;
+const multiply = num1 * num2;
+
+
+const  random =Math.floor(Math.random() * 10);
+//console.log(random);
+
+
+const num3 = 29;
+const num4 = 5;
+const mod = num3 % num4;
+
+var max =  Math.max(12, 4, 7, 9, 18,20);
+
